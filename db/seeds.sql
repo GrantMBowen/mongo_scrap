c8cd3a305f18dcf543e234ddfa40d1176381e341
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2019-12-07 17:26:51 -0800

    working on mongoose connection

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-/* insert at least three entries */
-
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Teriyaki');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Onion', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Boca', true);
-INSERT INTO burgers (burger_name) VALUES ('BBQ');
